4a6b687a23fc44c7a47164406b2aca5036ef6311
Willian Rigo <w9rigo6@gmail.com>
2025-08-04 14:38:48 +0200

    creating code for "onValueCreated" trigger

---
+++ backend/functions/src/types.ts
@@ -0,0 +1,24 @@
+export interface IUser {
+    userId: string,
+    name: string,
+    email: string,
+    zip: string,
+    lastUpdated: Date,
+    lastRequestId: string,
+    geoData?: IGeoData
+}
+
+export interface IGeoData {
+    lat: string,
+    lon: string,
+    timezone: string,
+    cityName: string
+}
+
+export interface IRequest {
+    status: 'success' | 'error',
+    errorCode?: 'INVALID_ZIP' | 'MISSING_REQ_ATTR' | 'INVALID_API_KEY' | 'GENERIC_ERROR',
+    errorMessage?: string,
+    requesterUserId: string,
+    requestId: string
+}
